eeaa17d8aedb0bc4e6680bbb539d9f51f931616f
marble-git <516018505@qq.com>
2021-01-17 06:55:26 +0800

    add force.py learn import <module> int and float class use <python3 file.py> to run a python3 program.

---
+++ force.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+'''
+my first program.
+filename: force.py
+'''
+
+import math
+f1 = 20
+f2 = 10
+alpha = math.pi / 3
+
+x_force = f1+f2*math.sin(alpha)
+y_force = f2*math.cos(alpha)
+
+force = math.sqrt(x_force * x_force + y_force ** 2 )
+
+print("The result is: ",round(force,2),'N')
+
