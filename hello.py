5886f6317201767261a23de0988f0d92b7b2cd1e
marble-git <516018505@qq.com>
2021-01-17 04:14:19 +0800

    add hello.py my first python program.

---
+++ hello.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+
+'''
+    This is my first python program.
+    I will be a great programmer.
+'''
+
+print("hello word") #print English word.
+print("你好 世界")  #print Chinese word.
+
