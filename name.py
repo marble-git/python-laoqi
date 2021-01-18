5f6355f85b7809f3034888f63871d35c48a28cea
marble-git <516018505@qq.com>
2021-01-19 05:13:09 +0800

    add name.py str 类型 的对象 不能与 int 类型对象 相加 +

---
+++ name.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+'''
+your name and age.
+'''
+
+name = input('your name: ')
+age = input('your age: ')
+
+after = int(age) + 10
+
+print('your name is: ',name)
+print('after ten years, you are ',after)
+
